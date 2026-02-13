c675c5105b0f43f6a49ee07c48dc68928fce7c2c
Mohdkaif379 <mohdkaif8603@gmail.com>
2026-02-13 16:13:57 +0530

    update controller

--- src/models/studentModel.js
+++ src/models/studentModel.js
@@ -7,13 +7,13 @@ exports.createStudent = (data, callback) => {
 
 exports.findAll = (callback) => {
   const sql =
-    "SELECT id, name, roll_no, email, phone, dob, gender, profile_image, father_name, father_phone, father_email, mother_name, mother_phone, mother_email, address_line1, address_line2, city, state, postal_code, country, class_id, status, created_at, updated_at FROM students ORDER BY id DESC";
+    "SELECT s.id, s.name, s.roll_no, s.email, s.phone, s.dob, s.gender, s.profile_image, s.father_name, s.father_phone, s.father_email, s.mother_name, s.mother_phone, s.mother_email, s.address_line1, s.address_line2, s.city, s.state, s.postal_code, s.country, s.class_id, c.class AS class_name, s.status, s.created_at, s.updated_at FROM students s LEFT JOIN classes c ON c.id = s.class_id ORDER BY s.id DESC";
   db.query(sql, callback);
 };
 
 exports.findById = (id, callback) => {
   const sql =
-    "SELECT id, name, roll_no, email, phone, dob, gender, profile_image, father_name, father_phone, father_email, mother_name, mother_phone, mother_email, address_line1, address_line2, city, state, postal_code, country, class_id, status, created_at, updated_at FROM students WHERE id = ?";
+    "SELECT s.id, s.name, s.roll_no, s.email, s.phone, s.dob, s.gender, s.profile_image, s.father_name, s.father_phone, s.father_email, s.mother_name, s.mother_phone, s.mother_email, s.address_line1, s.address_line2, s.city, s.state, s.postal_code, s.country, s.class_id, c.class AS class_name, s.status, s.created_at, s.updated_at FROM students s LEFT JOIN classes c ON c.id = s.class_id WHERE s.id = ?";
   db.query(sql, [id], callback);
 };
 
